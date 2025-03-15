4e51f377325bc97137376af91d8880d4878aff15
Jalileh <yacoub.m94f@gmail.com>
2025-03-15 18:13:08 +0100

    tmux stuff

--- essentials/tools/choco-installs.ps1
+++ essentials/tools/choco-installs.ps1
@@ -2,8 +2,12 @@ choco install git.install --version 2.47.1 -y
 choco install nerd-fonts-3270 --version 3.2.1 -y
 
 choco install mingw --version 13.2.0 -y
+choco install msys2 -y
+choco install wezterm -y
+
 choco install python -y 
 choco install golang -y
+
 choco install make -y 
 choco install go-task -y
 choco install nodejs.install -y
---
+++ media/.wezterm.lua
@@ -0,0 +1,7 @@
+local wezterm = require 'wezterm';
+
+return {
+	font = wezterm.font('FiraCode Nerd Font'),
+	default_prog = { 'c:/PROGRA~1/Git/bin/bash.exe' },
+	enable_tab_bar = false,
+}
